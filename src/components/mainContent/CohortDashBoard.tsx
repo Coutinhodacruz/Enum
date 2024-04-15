8362748955ef083feee2385ee146bb1ecbb71e17
onukwufor dominic <dominicrotimi@gmail.com>
2024-04-15 12:03:10 -0400

    changes made

--- src/components/mainContent/CohortDashBoard.tsx
+++ src/components/mainContent/CohortDashBoard.tsx
@@ -1,7 +1,3 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import { ViewAllCohortUrl } from "@/assets/urls/urls";
-import { FiUser } from "react-icons/fi";
 import CustomInput from "@/input/CustomInput";
 import { LuSearch } from "react-icons/lu";
 import Picture from "@/assets/Picture";
@@ -11,13 +7,17 @@ import CreateCohortModal from "../modal/CreateCohortModal";
 import { HiOutlineDotsVertical } from "react-icons/hi";
 import MoreActionButtonStyles from "../button/buttonStyles/MoreAction";
 import CohortAvatarStyle from "@/assets/style/CohortAvatarStyle";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { ViewAllCohortUrl } from "@/assets/urls/urls";
+import { FiUser } from "react-icons/fi";
 import style from './style/CohortDashBoardStyle.module.css'
 
 interface CohortDashBoardProps {
   handleOpen: any;
   clicked: boolean;
   handleClose: any;
-} 
+}
 
 interface CohortData {
   cohortName: string;
@@ -32,17 +32,19 @@ interface CohortData {
 }
 
 const CohortDashBoard: React.FC<CohortDashBoardProps> = ({
-  handleOpen,
-  clicked,
-  handleClose,
-}) => {
+                                                           handleOpen,
+                                                           clicked,
+                                                           handleClose,
+                                                         }) => {
   const [cohortData, setCohortData] = useState<CohortData[]>([]);
   const [searchQuery, setSearchQuery] = useState<string>("");
-  const [screenHeight, setScreenHeight] = useState<number | undefined>(undefined);
+  const [screenHeight, setScreenHeight] = useState<number | undefined>(
+      undefined
+  );
   const [screenWidth, setScreenWidth] = useState<number | undefined>(undefined);
 
   useEffect(() => {
-    if (typeof window !== 'undefined') {
+    if (typeof window !== "undefined") {
       setScreenHeight(window.innerHeight);
       setScreenWidth(window.innerWidth);
 
@@ -57,11 +59,15 @@ const CohortDashBoard: React.FC<CohortDashBoardProps> = ({
     }
   }, []);
 
+  const dataExists =
+      typeof cohortData === "object" &&
+      Object.values(cohortData).some((value) => value !== null);
+
   useEffect(() => {
     const fetchCohort = async () => {
       try {
         const token =
-          "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJyb2xlcyI6WyJBRE1JTiJdLCJpc3MiOiJFbnVtIn0.aNaQX6099P1v9E67yUfxznob9bAQDWDWhEUCRgrgMKDxUMqZAEsYVIWJji3VwgrWaDrtQNNWpHjgpF8mgobEHg";
+            "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJyb2xlcyI6WyJBRE1JTiJdLCJpc3MiOiJFbnVtIn0.aNaQX6099P1v9E67yUfxznob9bAQDWDWhEUCRgrgMKDxUMqZAEsYVIWJji3VwgrWaDrtQNNWpHjgpF8mgobEHg";
         const response = await axios.get(ViewAllCohortUrl, {
           method: "GET",
           headers: {
@@ -69,6 +75,7 @@ const CohortDashBoard: React.FC<CohortDashBoardProps> = ({
           },
         });
         setCohortData(response.data);
+        console.log("response ---->", response.data);
       } catch (e: any) {
         console.log(e.error);
       }
@@ -76,103 +83,105 @@ const CohortDashBoard: React.FC<CohortDashBoardProps> = ({
     fetchCohort();
   }, []);
 
-  const filteredCohorts = cohortData.filter(cohort =>
-    cohort.cohortName.toLowerCase().includes(searchQuery.toLowerCase()))
+  const filteredCohorts = cohortData.filter((cohort) =>
+      cohort.cohortName.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
   return (
-    <>
-      <div className="h-screen md:w-full md:text-blue ">
-        <div className="flex pt-5 gap-5 flex-col-reverse md:flex md:flex-row mt-5 md:justify-between w-[100%] ">
-          <div className="flex md:w-[400px] sm:h-[12vh] md:h-[7vh] border border-solid border-grey-100 gap-2 rounded-[7px] justify-start items-center pl-2">
-            <LuSearch color={"#D0DCE4"} />
-            <input
-              placeholder="Search"
-              style={{ width: "360px", border: "none", outline: "none", boxShadow: "none" }}
-              type={"text"}
-              value={searchQuery}
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) => setSearchQuery(e.target.value)}
-            />
+      <>
+        <div className="w-[300px] md:w-full md:text-blue">
+          <div className="flex pt-5 gap-5 flex-col-reverse md:flex md:flex-row mt-5 md:justify-between w-[100%]">
+            <div className="flex md:w-[400px] sm:h-[12vh] md:h-[7vh] border border-solid border-gray-100 gap-2 rounded-[7px] justify-start items-center pl-2">
+              <LuSearch color={"#D0DCE4"} />
+              <input
+                  placeholder="Search"
+                  style={{
+                    width: "360px",
+                    border: "none",
+                    outline: "none",
+                    boxShadow: "none",
+                  }}
+                  type="text"
+                  value={searchQuery}
+                  onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                      setSearchQuery(e.target.value)
+                  }
+              />
+            </div>
+
+            <div className="flex flex-row gap-4">
+              <CustomButton
+                  icons={[]}
+                  text={"Create a cohort"}
+                  style={createCohortStyles}
+                  onClick={handleOpen}
+                  isDisabled={false}
+              />
+              <CustomButton
+                  icons={[<HiOutlineDotsVertical key="dots-icon" />]}
+                  text={"More Actions"}
+                  style={MoreActionButtonStyles}
+                  isDisabled={false}
+              />
+            </div>
           </div>
-
-          <div className="flex flex-row  gap-4">
-            <CustomButton
-              icons={[]}
-              text={"Create a cohort"}
-              style={createCohortStyles}
-              onClick={handleOpen}
-              isDisabled={false}
-            />
-            <CustomButton
-              icons={[<HiOutlineDotsVertical key="dots-icon" />]}
-              text={"More Actions"}
-              style={MoreActionButtonStyles}
-              isDisabled={false}
-
-            />
-          </div>
-        </div>
-        <div className="h-4/6">
-          
-          <div className={`flex flex-col overflow-x-hidden mt-8 md:w-full md:max-w-[100%] max-h-full ${style.cohortList}  sm:h-full`}
-
+          <div
+              className={`flex flex-col overflow-x-hidden md:w-full md:max-w-[100%] ${style.cohortList} sm:h-full mt-0 mb-0 ml-0 mr-0 pt-0 pb-0 pl-0 pr-0`}
+              style={{ maxHeight: 'calc(100vh - 370px)', overflowY: 'auto' }}
           >
-            
             {filteredCohorts.length > 0 ? (
-              filteredCohorts.map((item, index) => (
-                <div
-                  key={index}
-                  className="flex flex-row shadow-inner justify-between mb-5 p-2 h-[70px] items-center md:w-full md:max-w-[100%]  border-[#F6FCF] rounded-lg "
-                >
-                  <div className="flex gap-3 justify-center items-center">
-                    <div className="flex justify-center items-center">
-                      <Picture url={item.avatarImageUrl} style={CohortAvatarStyle} />
-                    </div>
-
-                    <div className="flex flex-col gap-1 w-full">
-                      <div className="font-DM Sans text-sm font-semibold">{item.cohortName}</div>
-                      <div className="flex items-center gap-2">
-                        <div className="flex flex-wrap gap-2 w-64">
-                          {item.programs.map((program, itemIndex) => (
-                            <span
-                              key={itemIndex}
-                              className="font-medium text-sm text-gray-600 px-2 py-1 rounded-md"
-                            >
-                              {program.programName}
-                            </span>
-                          ))}
+                filteredCohorts.map((item, index) => (
+                    <div
+                        key={index}
+                        className="flex flex-row shadow-inner justify-between mb-5 p-2 h-[70px] items-center md:w-full md:max-w-[100%] border-[#F6FCF] rounded-lg "
+                    >
+                      <div className="flex gap-3 items-center">
+                        <div className="flex justify-center items-center">
+                          <Picture
+                              url={item.avatarImageUrl}
+                              style={CohortAvatarStyle}
+                          />
                         </div>
-                        <div className="flex-grow flex items-center gap-2">
-                          <FiUser className="w-4 h-4 text-gray-400" />
-                          <span className="font-medium text-sm text-gray-600">
-                            25 Learners
+                        <div className="flex flex-col gap-1 w-full">
+                          <div className="font-DM Sans text-sm font-semibold">{item.cohortName}</div>
+                          <div className="flex items-center gap-2">
+                            <div className="flex flex-wrap w-60">
+                              {item.programs.map((program, itemIndex) => (
+                                  <span
+                                      key={itemIndex}
+                                      className="font-sm text-sm text-gray-800 py-1 rounded-md"
+                                  >
+                            {program.programName}
                           </span>
+                              ))}
+                            </div>
+                            <div className="flex-grow flex items-center gap-2">
+                              <FiUser className="w-4 h-4 text-gray-400" />
+                              <span className="font-sm text-sm text-gray-800">
+                          25 Learners
+                        </span>
+                            </div>
+                          </div>
                         </div>
                       </div>
+                      <div className="flex justify-center items-center gap-8 pr-7 font-dm sans">
+                        <div className="flex items-center gap-2 text-sm">
+                          <div>Created</div>
+                          <div>{item.startDate}</div>
+                        </div>
+                        <HiOutlineDotsVertical />
+                      </div>
                     </div>
-                  </div>
-                  <div className="flex justify-center items-center gap-8 pr-7 font-dm sans">
-                    <div className="flex items-center gap-2 text-sm">
-                      <div>Created</div>
-                      <div>{item.startDate}</div>
-                    </div>
-                    <HiOutlineDotsVertical />
-                  </div>
-                </div>
-              ))
+                ))
             ) : (
-              <div className="text-center mt-8">No Suggestions Exist!!!</div>
+                <div className="text-center mt-8">No Suggestions Exist!!!</div>
             )}
           </div>
-        </div>
 
-        <CreateCohortModal isClicked={clicked} closeModal={handleClose} />
-      </div>
-    </>
+          <CreateCohortModal isClicked={clicked} closeModal={handleClose} />
+        </div>
+      </>
   );
 };
 
 export default CohortDashBoard;
-
-
-
-//  style={screenHeight ? { height: `calc(${screenHeight}px - 50px)` } : {}}
-// style={{ height: screenHeight ? `calc(${screenHeight}px - 90px)` : "auto", overflowY: "auto" }}
